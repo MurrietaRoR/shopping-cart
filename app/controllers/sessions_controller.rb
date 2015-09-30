10caa452fe4caeac5a0e6d3c25847d161e63ef3d
Pisei Lim <piseilim@Piseis-MBP.home>
2015-09-30 11:46:51 -0700

    Added alert messages to main application layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,18 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to root_url
+      flash[:success] = "Logged in!"
     else
-      redirect_to 'login'
+      flash[:danger] = "Email or password is invalid."
+      render "new"
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/'
+    redirect_to root_url
+    flash[:success] = "Logged out!"
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to root_url, notice: "Thank you for signing up!"
     else
-      redirect_to '/signup'
+      flash[:warning] = 'Email has already been taken.' # Not quite right!
+      render "new"
     end
   end
 
