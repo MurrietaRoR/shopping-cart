6f58835d6acf972b00cd5dea47f2895f8a678c91
Bryan Allen <bryanallen.socal@gmail.com>
2015-10-31 10:16:55 -0700

    Shopping cart updates products  with orders. The cart link also updates via ajax. Need to display shopping cart with shipping and tax info before finalizing order.
    Need to display products on 1 row combined. Clean this up.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,12 @@
 class Order < ActiveRecord::Base
   has_many :order_items
-  
+
   before_save :update_subtotal
-  
-  #validates :subtotal, presence: true
-  validates :tax, presence: true
-  validates :shipping, presence: true
-  validates :total, presence: true
+
+  validates :subtotal, presence: true
+  #validates :tax, presence: true
+  #validates :shipping, presence: true
+  #validates :total, presence: true
 
   enum status: [ :cancelled, :in_progress, :completed, :invoiced ]
 
